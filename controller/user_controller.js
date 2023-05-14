fe368efafeef6ec5a32e43fcfe24b1e6f18b2d71
tush87 <tusharj8788@gmail.com>
2023-05-14 14:25:42 +0530

    add user create functionality

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,22 +1,45 @@
+const UserDB = require('../models/user');
 
 //create user
 module.exports.create=async function(req,res){
     try{
         //password and confirm password not match
         if(req.body.password != req.body.confirmPassword){
-            return res.status(400).josn({
+            return res.status(400).json({
                 message:"Password and Confirm Password not match"
             })
         }
         //check user already exist in db
-        // let user = await 
-        
+        let user = await UserDB.findOne({email:req.body.email});
+
+        //if user not found 
+        if(!user){
+            user = await UserDB.create(req.body);
+            return res.status(200).json({
+                message:"User Create Successfully"
+            })
+        }
+
+        //if user already exist then just back
+        return res.status(200).json({
+            message:"User Already exist"
+        })        
         
     }
     catch(err){
-        console.log(err);
-        return res.status(500).josn({
-            message:"Internal Server Error"
+        let errorMessage,code;
+        if(err.name=="ValidationError"){
+            errorMessage="Missing email,password or confirmPassword",
+            code=400
+        }
+        else{
+            errorMessage="Internal Server Error"
+            code=500;
+        }
+
+        // console.log(err.);
+        return res.status(code).json({
+            message:errorMessage
         })
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const homeController = require('../controller/home_controller');
 const router = express.Router();
 
+//home page
+router.get('/',homeController.home);
+
+//v1
 router.use('/v1',require('./v1/index'));
 
 module.exports=router;
