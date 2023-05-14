f58f1513a8749a92d02ae3819dc808fed366c8b6
tush87 <tusharj8788@gmail.com>
2023-05-14 20:10:30 +0530

    add home controller

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,3 +1,6 @@
+const path = require('path')
+
+//home page
 module.exports.home = function(req,res){
-    return res.end('<h1>Home Page</h1>')
+    return res.sendFile(path.join(__dirname,"../views/home.html"));
 }
