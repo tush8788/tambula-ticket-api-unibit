fa3e702877d2643325e4e01ee335187040e9f9b6
tush87 <tusharj8788@gmail.com>
2023-05-14 13:35:46 +0530

    add router for user

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = process.env.PORT||8000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/',require('./routes/index'));
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(`server is up on port ${port}`);
+})
